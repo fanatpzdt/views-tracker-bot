2ce21bfbc1f81518fce203f0af6650b0d1dd02dc
fanatpzdt <borovichnicely@gmail.com>
2026-07-28 14:12:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ TOKEN = "8206628983:AAHhyn26UBXgGwOEiD49_399KPASmsRD30I"
 
 bot = telebot.TeleBot(TOKEN)
 
-# создаём базу
 db = sqlite3.connect("videos.db", check_same_thread=False)
 cursor = db.cursor()
 
@@ -14,6 +13,8 @@ cursor.execute("""
 CREATE TABLE IF NOT EXISTS videos (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     link TEXT,
+    platform TEXT,
+    views INTEGER DEFAULT 0,
     date TEXT
 )
 """)
@@ -25,7 +26,7 @@ db.commit()
 def start(message):
     bot.send_message(
         message.chat.id,
-        "Привет! Отправь мне ссылку на TikTok или YouTube Shorts."
+        "Привет! Отправь ссылку на TikTok или YouTube Shorts."
     )
 
 
@@ -33,42 +34,51 @@ def start(message):
 def save_link(message):
     link = message.text
 
-    if "tiktok.com" not in link and "youtube.com" not in link and "youtu.be" not in link:
+    if "tiktok.com" in link:
+        platform = "TikTok"
+    elif "youtube.com" in link or "youtu.be" in link:
+        platform = "YouTube"
+    else:
         bot.send_message(
             message.chat.id,
-            "❌ Отправь только ссылку на TikTok или YouTube Shorts"
+            "❌ Нужна ссылка TikTok или YouTube Shorts"
         )
         return
 
     date = datetime.now().strftime("%d.%m.%Y")
 
     cursor.execute(
-        "INSERT INTO videos (link, date) VALUES (?, ?)",
-        (link, date)
+        "INSERT INTO videos (link, platform, date) VALUES (?, ?, ?)",
+        (link, platform, date)
     )
 
     db.commit()
 
-    cursor.execute("SELECT COUNT(*) FROM videos")
-    count = cursor.fetchone()[0]
-
     bot.send_message(
         message.chat.id,
         f"✅ Сохранил!\n"
-        f"Дата: {date}\n"
-        f"Всего роликов: {count}"
+        f"Платформа: {platform}\n"
+        f"Дата: {date}"
     )
 
 
 @bot.message_handler(commands=['stats'])
 def stats(message):
-    cursor.execute("SELECT COUNT(*) FROM videos")
-    count = cursor.fetchone()[0]
+    cursor.execute("""
+    SELECT platform, COUNT(*) 
+    FROM videos 
+    GROUP BY platform
+    """)
 
-    bot.send_message(
-        message.chat.id,
-        f"📊 Всего добавлено роликов: {count}"
-    )
+    result = cursor.fetchall()
+
+    text = "📊 Статистика:\n"
+
+    for platform, count in result:
+        text += f"{platform}: {count} роликов\n"
+
+    bot.send_message(message.chat.id, text)
 
 
 bot.infinity_polling()
+
